95f8b712f1443e7cd2ae919f6e4602eaa18e9440
Kenneth Aalberg <kenneaal@gmail.com>
2018-07-12 16:32:47 +0200

    Base model example for JIRA.

--- jiraannouncer/models/mymodel.py
+++ jiraannouncer/models/mymodel.py
@@ -3,9 +3,21 @@ from sqlalchemy import (
     Index,
     Integer,
     Text,
+    BigInteger,
+    JSON,
 )
 
 from .meta import Base
+
+
+class JIRAMessage(Base):
+    __tablename__ = 'jiramessage'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    timestamp = Column(BigInteger)
+    event = Column(Text)
+    user = Column(JSON)
+    issue = Column(JSON)
+    changelog = Column(JSON)
 
 
 class MyModel(Base):
