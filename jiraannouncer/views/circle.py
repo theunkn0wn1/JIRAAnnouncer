d3ee4b6aaf67b142098921d1dc2d7c108bca7326
Kenneth Aalberg <kenneaal@gmail.com>
2019-06-02 17:42:07 +0200

    Don't clobber request, pull body to JSON data.

--- jiraannouncer/views/circle.py
+++ jiraannouncer/views/circle.py
@@ -1,4 +1,5 @@
 import time
+import simplejson
 
 from pyramid.view import view_config
 
@@ -11,37 +12,37 @@ OFFSET = 5
 def circle(request):
     """Handle CircleCI events"""
     lastmessage = getlast()
-
-    if 'reponame' not in request.params:
+    data = simplejson.loads(request.body)
+    if 'reponame' not in data:
         logprint("No repository name in request!")
     else:
-        if request.params['reponame'] == "pipsqueak3":
-            if request.params['username'] == "FuelRats":
+        if data['reponame'] == "pipsqueak3":
+            if data['username'] == "FuelRats":
                 channels = ['#mechadev']
             else:
-                jsondump(request)
+                jsondump(data)
                 return
         else:
             channels = ['#rattech']
 
-    if request.params['compare'] is None:
-        if len(request.params['all_commit_details']) > 0:
-            compareurl = request.params['all_commit_details'][0]['commit_url']
-        elif len(request.params['pull_requests']) > 0:
-            compareurl = request.params['pull_requests'][0]['url']
+    if 'compare' not in data:
+        if len(data['all_commit_details']) > 0:
+            compareurl = data['all_commit_details'][0]['commit_url']
+        elif len(data['pull_requests']) > 0:
+            compareurl = data['pull_requests'][0]['url']
         else:
             compareurl = "\x0302null\x03"
     else:
-        compareurl = request.params['compare']
+        compareurl = data['compare']
 
-    message1 = ("[\x0315CircleCI\x03] \x0306" + request.params['reponame'] + "/" + request.params['reponame'] +
-                "\x03#" + str(request.params['build_num']) + " (\x0306" + request.params['branch'] + "\x03 - " +
-                request.params['vcs_revision'][:7] + " : \x0314" + request.params['user']['login'] +
-                "\x03): " + request.params['outcome'])
+    message1 = ("[\x0315CircleCI\x03] \x0306" + data['reponame'] + "/" + data['reponame'] +
+                "\x03#" + str(data['build_num']) + " (\x0306" + data['branch'] + "\x03 - " +
+                data['vcs_revision'][:7] + " : \x0314" + data['user']['login'] +
+                "\x03): " + data['outcome'])
     message2 = ("[\x0315CircleCI\x03] Change view: \x02\x0311" + compareurl +
-                "\x02\x03 Build details: \x02\x0311" + request.params['build_url'] + "\x02\x03")
-    msgshort1 = {"time": time.time(), "type": "Circle", "key": request.params['reponame'], "full": message1}
-    msgshort2 = {"time": time.time(), "type": "Circle", "key": request.params['reponame'], "full": message2}
+                "\x02\x03 Build details: \x02\x0311" + data['build_url'] + "\x02\x03")
+    msgshort1 = {"time": time.time(), "type": "Circle", "key": data['reponame'], "full": message1}
+    msgshort2 = {"time": time.time(), "type": "Circle", "key": data['reponame'], "full": message2}
     if lastmessage['full'] == message2:
         logprint("Duplicate message, skipping:")
         logprint(message1)
